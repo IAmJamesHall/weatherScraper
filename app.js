785d4125ac6f4fae1dd750b71d30d8117d29765b
James Hall <hi@jameshall.xyz>
2022-12-20 12:09:43 -0500

    moved module to new modules folder

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const rp = require('request-promise');
-const extractWeatherDataFromHTML = require('./extractWeatherDataFromHTML');
+const extractWeatherDataFromHTML = require('./modules/extractWeatherDataFromHTML');
 // const puppeteer = require('puppeteer');
 // const url = "https://www.accuweather.com/en/us/endicott/13760/december-weather/1055";
 const url = "https://weather.com/weather/tenday/l/240f381aa3ed298a691a657f8012d0f226d176e45df5a3fec59c1c342926d3a3"; //tenday weather forecast from weather.com
