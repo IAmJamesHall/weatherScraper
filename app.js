2a5a6daa0477f3537d981ffb1aa5b28a4d22b8e2
James Hall <hi@jameshall.xyz>
2022-12-21 11:02:43 -0500

    finish writing weather scrapers

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const rp = require('request-promise');
+const request = require('request-promise');
 const extractWeatherDataFromHTML = require('./modules/extractWeatherDataFromHTML');
 // const puppeteer = require('puppeteer');
 // const url = "https://www.accuweather.com/en/us/endicott/13760/december-weather/1055";
 const url = "https://weather.com/weather/tenday/l/240f381aa3ed298a691a657f8012d0f226d176e45df5a3fec59c1c342926d3a3"; //tenday weather forecast from weather.com
 
-console.log('Starting function');
-rp(url)
+
+request(url)
     .then(html => {
-        console.log('Success');
+        console.log('Starting function');
         extractWeatherDataFromHTML(html);
     });
 
--- modules/extractWeatherDataFromHTML.js
+++ modules/extractWeatherDataFromHTML.js
@@ -3,45 +3,77 @@ const cheerio = require('cheerio');
 
 
 const extractWeatherDataFromHTML = (html) => {
-    const weatherData = {};
+    const weatherArray = [];
     const $ = cheerio.load(html);
     for (let i = 1; i < 15; i += 1) {
-        const weatherDetail = $(`details#detailIndex${i}`);
+        const weatherDetail = $(`details#detailIndex${i}`).html();
         
         //TODO: figure out how to put the info inside an object, and organize by date
         const date = extractDate(weatherDetail);
-        extractTemps(weatherDetail);
+        const temps = extractTemps(weatherDetail);
+        const condition = extractCondition(weatherDetail);
+        const precipitationChance = extractPrecipitationChance(weatherDetail);
+
+        weatherArray.push({ date, 
+                            highTemp: temps.highTemp, 
+                            lowTemp: temps.lowTemp,
+                            condition,
+                            precipitationChance
+                        });
+
+        
     }
-    // console.log($('details#detailIndex1').text());
+    console.log(weatherArray);
+
 }
 
 const extractDate = (weatherDetail) => {
-    const $ = cheerio.load(weatherDetail.html());
+    const $ = cheerio.load(weatherDetail);
     const day = Number($('h3', weatherDetail).text().slice(4, 6)); // day of the month (e.g. 14)
 
     let dateString;
     //get current day of the month
     const d = new Date;
 
+    
+
     const thisMonth = d.getMonth() + 1; //add 1 to the current month number (Date() is zero-based)
     let nextMonth = d.getMonth() + 2;  //add 2 to the month number (zero-based)
-    if (nextMonth == 13) nextMonth = 1;
+
+    let year = d.getFullYear()
+
+    if (nextMonth == 13) { //handling December/January nonsense
+        nextMonth = 1;
+        year += 1;
+    }
+
+
     if (d.getDate() < day) {
-        dateString = `${thisMonth}/${day}` 
+        dateString = `${year}/${thisMonth}/${day}` 
     } else if (d.getDate() > day) {
-        dateString = `${nextMonth}/${day}`
+        dateString = `${year}/${nextMonth}/${day}`
     }
 
     return dateString;
 }
 
 const extractTemps = (weatherData) => {
-    const $ = cheerio.load(weatherData.html());
+    const $ = cheerio.load(weatherData);
     let temps = $("div[data-testid='detailsTemperature']").text();
     const highTemp = temps.match(/([0-9]+)°\//)[1];
     const lowTemp = temps.match(/\/([0-9]+)°/)[1];
 
     return({ highTemp, lowTemp });
+}
+
+const extractCondition = (weatherData) => {
+    const $ = cheerio.load(weatherData);
+    return $('.DetailsSummary--condition--2JmHb span').text();
+}
+
+const extractPrecipitationChance = (weatherData) => {
+    const $ = cheerio.load(weatherData);
+    return $('.DetailsSummary--precip--1a98O span').text();
 }
 
 
